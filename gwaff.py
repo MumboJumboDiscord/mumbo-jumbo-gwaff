ac3f63ae33f9aa22c7f452967248febd5d6e2bcf
bwac <40532058+bwac2517@users.noreply.github.com>
2020-07-11 19:37:32 +1000

    Yes.

--- gwaff.py
+++ gwaff.py
@@ -2,17 +2,30 @@ from scripts import plot, data
 import datetime
 import json
 import argparse
+import requests
+import os
 
 
 parser = argparse.ArgumentParser(description="mee6 xp graphing")
-parser.add_argument("-p", help="Use this if you want to plot", action="store_true")
-parser.add_argument("-s", help="Use this if you want to store", action="store_true")
+parser.add_argument("--plot", help="Use this if you want to plot", action="store_true")
+parser.add_argument("--store", help="Use this if you want to store", action="store_true")
 parser.add_argument("--save", help="Use this if you want to store the generated plots", action="store_true")
+parser.add_argument("--post", help="Use this if you want to post the generated plots, takes a url (str)", type=str)
 args = parser.parse_args()
 
 
-def save():
-    print("Saving...")
+def post(url: str):
+    print("Posting...")
+    for filename in os.listdir("images/"):
+        image = open(f"images/{filename}", "rb")
+        requests.post(url=url, json={"embeds": [{"title": "title", "thumbnail": {"url": "https://raw.githubusercontent"
+                                                                                        ".com/bwac2517/gwaff/master"
+                                                                                        "/assets/icon.png"},
+                                                 "image": {"url": "attachment://image.png"}}]}, files={"image.png": image})
+
+
+def store():
+    print("Storing...")
     new_data = data.get()
     time = datetime.datetime.today()
 
@@ -27,7 +40,7 @@ def plot_(save: bool = False):
     with open("gwaff.json", "r") as outfile:
         gwaff = json.load(outfile)
 
-    if parser.parse_args().save:
+    if args.save:
         plot.bar(gwaff, save=True)
         plot.line(gwaff, save=True)
     else:
@@ -35,18 +48,20 @@ def plot_(save: bool = False):
         plot.line(gwaff)
 
 
-if parser.parse_args().s:
-    save()
-elif parser.parse_args().p:
-    plot_()
+if args.store:
+    store()
+if args.plot:
+    if args.save:
+        plot_(True)
+    else:
+        plot_()
+if type(args.post) == str:
+    post(args.post)
 else:
     i = input("No flag slected, what do you want to do? (p or s?) >")
     if i == "p":
-        if parser.parse_args().save:
-            plot_(True)
-        else:
-            plot_()
+        plot_()
     elif i == "s":
-        save()
+        store()
     else:
         raise Exception("No flags or valid input")
